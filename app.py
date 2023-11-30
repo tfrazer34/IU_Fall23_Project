be3df42aee3001bfdf830b6cc47ba26aeabc0101
Kamil Kosidlak <kkosidlak@gmail.com>
2023-11-30 18:18:24 -0500

    fixed prediction error

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", prediction=None)
 
 @app.route('/predict', methods=["POST"])
 def predict():
