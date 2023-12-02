309dbcac3f4906de39549e353a465c1920c6aed4
Alexis Perez <ap70@iu.edu>
2023-12-02 14:17:17 -0500

    Adding visuals tab

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt 
+from io import BytesIO 
+import base64 
 
 
 #create flask app
@@ -18,6 +21,30 @@ model = pickle.load(open("model.pkl", "rb"))
 @app.route('/')
 def home():
     return render_template("index.html", prediction=None)
+
+@app.route('/visuals') 
+
+def visuals(): 
+    # Assuming 'date' is in datetime format, if not, convert it using pd.to_datetime() 
+    last_week_data = weather_df.tail(7) 
+    
+    # Plotting 
+    plt.figure(figsize=(10, 6)) 
+    plt.plot(last_week_data['date'], last_week_data['weather'], marker='o') 
+    plt.title('Weather for the Last 7 Days') 
+    plt.xlabel('Date') 
+    plt.ylabel('Weather') 
+    plt.xticks(rotation=45) 
+    
+    # Save plot to a BytesIO object 
+    img = BytesIO() 
+    plt.savefig(img, format='png') 
+    img.seek(0) 
+    
+    # Encode the plot to base64 for embedding in HTML 
+    plot_url = base64.b64encode(img.getvalue()).decode() 
+    
+    return render_template('visuals.html', plot_url=plot_url)
 
 @app.route('/predict', methods=["POST"])
 def predict():
