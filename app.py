692d7a936007da51a0ad0c3aed800dae06c1e5a5
Kamil Kosidlak <kkosidlak@gmail.com>
2023-12-08 22:28:39 -0500

    moving website branch to main

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from io import BytesIO 
+import base64 
+
+
+#create flask app
 
 app = Flask(__name__)
 
+
+# load the pickled model
+
+model = pickle.load(open("model.pkl", "rb"))
+
+
+#create a flask endpoints
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("index.html", prediction=None)
+
+weather_df = pd.read_csv('data/seattle-weather.csv')
+@app.route('/visuals') 
+def visuals(): 
+    # Assuming 'date' is in datetime format, if not, convert it using pd.to_datetime() 
+    last_week_data = weather_df.tail(7) 
+    
+    # Plotting 
+    plt.figure(figsize=(10, 6)) 
+    plt.plot(last_week_data['date'], last_week_data['weather'], marker='o') 
+    plt.title('Weather for the Last 7 Days') 
+    plt.xlabel('Date') 
+    plt.ylabel('Weather') 
+    plt.xticks(rotation=45) 
+    
+    # Save plot to a BytesIO object 
+    img = BytesIO() 
+    plt.savefig(img, format='png') 
+    img.seek(0) 
+    
+    # Encode the plot to base64 for embedding in HTML 
+    plot_url = base64.b64encode(img.getvalue()).decode() 
+    
+    return render_template('visuals.html', plot_url=plot_url)
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    float_vals = [float(x) for x in request.form.values()]
+    vals = [np.array(float_vals)]
+
+    predictions = model.predict(vals)
+
+    return render_template("index.html", prediction = predictions)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-#test
---
+++ model.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+
+# Load csv file
+# df = pd.read_csv("Iris.csv")
+weather_df = pd.read_csv('data/seattle-weather.csv')
+
+#Select dependant and independant variables
+
+#X = df[['sepal_length','sepal_width','petal_length','petal_width']]
+#y = df['species']
+
+
+X = weather_df.drop(columns=['weather', 'date']) 
+y = weather_df['weather']
+
+#Train-test split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#Scaling features
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+#model instantiation
+
+#classifier = RandomForestClassifier()
+classifier = LogisticRegression(max_iter=1000)
+
+# fit model
+classifier.fit(X_train, y_train)
+
+# Make a pickle file for the trained model
+
+pickle.dump(classifier, open("model.pkl", 'wb'))
